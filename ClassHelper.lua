0f83eacfb34a738b58497127e35f00a4a91b77a1
Riissum <riissum11@hotmail.com>
2018-02-08 11:56:20 +0100

    Updates

--- ClassHelper.lua
+++ ClassHelper.lua
@@ -10,18 +10,45 @@ ClassHelperFrame:SetScript("OnEvent", function(self, ...)
 		ClassFrame = CreateFrame("Frame")
 
 		if (engClass == "PRIEST") then
-			print("Priest: OK")
+			print("|cff00daecClassHelper: |cff00ff00OK. |cffffffff(Priest)")
 			ACTIVE_BUFFS = { Voidform=0 }
 			ClassFrame:RegisterEvent("UNIT_AURA","player")
 			ClassFrame:SetScript("OnEvent", PriestHelper)
+		elseif (engClass == "PALADIN") then
+			print("|cff00daecClassHelper: |cff00ff00OK. |cffffffff(Paladin)")
+			ClassFrame:SetScript("OnUpdate", function(self, sinceLastUpdate) PaladinHelperBuffs(sinceLastUpdate); end)
 		else
-			print("|cffff0000 Mads: Unsupported class for ClassHelper. Terminates.")
+			print("|cff00daecClassHelper: Terminated.")
 			return
 		end
 	
 
 end)
 
+local timeSinceLastBuffAnnounce = { A=1000, B=1000 }
+
+function PaladinHelperBuffs(SLU) 
+	local ACTIVE_BUFFS = { A="Greater Blessing of Wisdom", B="Greater Blessing of Kings" }
+	
+	for k,v in pairs(ACTIVE_BUFFS) do
+		local _, _, _, count = UnitBuff("player", v)
+		timeSinceLastBuffAnnounce[k] = timeSinceLastBuffAnnounce[k] + SLU
+		local tt
+		if (count) then
+			if (timeSinceLastBuffAnnounce[k] >= 60) then
+				tt = date("[%H:%M]", time())
+				print(tt..": "..v.." Buff ok.")
+				timeSinceLastBuffAnnounce[k] = 0
+			end
+		elseif (count == nil) then
+			if (timeSinceLastBuffAnnounce[k] >= 60) then
+				tt = date("[%H:%M]", time())
+				print(tt..": "..v.." Buff missing.")
+				timeSinceLastBuffAnnounce[k] = 0
+			end
+		end
+	end
+end
 
 function PriestHelper(self, event, ...) 
 	local unit = ...
